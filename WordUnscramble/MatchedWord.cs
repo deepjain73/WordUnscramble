e8365bb38b8b4428f7017a77aa43642096bad692
deepti jain <deepti.jain@live.in>
2020-12-03 13:17:18 -0800

    Add project files.

---
+++ WordUnscramble/MatchedWord.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordUnscramble
+{
+    class MatchedWord
+    {
+        public string matchScrambledWord { get; set; }
+        public string matchWord { get; set; }
+    }
+}
---
+++ WordUnscramble/WordMatcher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordUnscramble
+{
+    class WordMatcher
+    {
+        public List<MatchedWord> Match(string[] scrambledWords, string[] wordlist)
+        {
+            var matchedWords = new List<MatchedWord>();
+            foreach (var scrambledWord in scrambledWords)
+            {
+                foreach (string word in wordlist)
+                {
+                    if (scrambledWord.Equals(word, StringComparison.OrdinalIgnoreCase))
+                    {
+                        matchedWords.Add(new MatchedWord()
+                        {
+                            matchScrambledWord = scrambledWord,
+                            matchWord = word
+                        });
+                    }
+                    else
+                    {
+                        var scrambledWordArray = scrambledWord.ToCharArray();
+                        var wordArray = word.ToCharArray();
+
+                        Array.Sort(scrambledWordArray);
+                        Array.Sort(wordArray);
+
+                        var sortedScrambledWord = new string(scrambledWordArray);
+                        var sortedWord = new string(wordArray);
+
+                        if (sortedScrambledWord.Equals(sortedWord, StringComparison.OrdinalIgnoreCase))
+                        {
+                            matchedWords.Add(new MatchedWord()
+                            {
+                                matchScrambledWord = sortedScrambledWord,
+                                matchWord = sortedWord
+
+                            });
+
+                        }
+                    }
+                }
+            }
+
+            return matchedWords;
+        }
+    }
+}
